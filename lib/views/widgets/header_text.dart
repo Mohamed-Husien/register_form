a170b12f47cb07f943ccd3e0c9faf1f3694cf9cf
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2024-12-24 10:58:32 +0200

    create register body and header text widgets

---
+++ lib/views/widgets/header_text.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class HeaderText extends StatelessWidget {
+  const HeaderText({
+    super.key,
+    required this.text1,
+    required this.text2,
+    required this.text3,
+  });
+  final String text1, text2, text3;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          text1,
+          style: const TextStyle(
+            color: Color(0xff2C385D),
+            fontSize: 48,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          text2,
+          style: const TextStyle(
+            color: Color(0xff1B2B51),
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          text3,
+          style: const TextStyle(
+            color: Color(0xff1B2B51),
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
+  }
+}
