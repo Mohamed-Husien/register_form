b6bafd47f1f1b65b7dc3143f899b180bd4d96902
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2025-01-26 09:03:00 +0200

    add auth states

--- lib/blocs/auth_bloc/auth_state.dart
+++ lib/blocs/auth_bloc/auth_state.dart
@@ -4,3 +4,27 @@ part of 'auth_bloc.dart';
 sealed class AuthState {}
 
 final class AuthInitial extends AuthState {}
+
+final class RegisterInitial extends AuthState {}
+
+final class RegisterLoading extends AuthState {}
+
+final class RegisterSuccess extends AuthState {}
+
+final class RegisterFailure extends AuthState {
+  final String erorrMessage;
+
+  RegisterFailure({required this.erorrMessage});
+}
+
+final class SignInitial extends AuthState {}
+
+final class SignInSuccess extends AuthState {}
+
+final class SignInLoading extends AuthState {}
+
+final class SignInFailure extends AuthState {
+  final String erorrMessage;
+
+  SignInFailure({required this.erorrMessage});
+}
