c791ba6a0438f7ebeb26047a3c305f9a3595807f
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2025-01-26 09:05:46 +0200

    here i add the events

--- lib/blocs/auth_bloc/auth_event.dart
+++ lib/blocs/auth_bloc/auth_event.dart
@@ -2,3 +2,15 @@ part of 'auth_bloc.dart';
 
 @immutable
 sealed class AuthEvent {}
+
+class LoginEvent extends AuthEvent {
+  final String email, password;
+
+  LoginEvent({required this.email, required this.password});
+}
+
+class RegisterEvent extends AuthEvent {
+  final String email, password;
+
+  RegisterEvent({required this.email, required this.password});
+}
